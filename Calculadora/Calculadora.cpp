2564d10b876e2ea1e1f97f90e5b5d6742d719bf7
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-09-15 09:38:25 -0500

    switch - If
    We deleted switch and changed it for if. Still need  to fix the results

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -5,7 +5,75 @@ e-mail: sofiapalacioscuevas@comunidad.unam.mx
 ///////////////// PRACTICA 3: CALCULADORA ////////////////////////////
 */
 
+/* pseudocode
+
+procedure calulate(n1,oper,n2)
+    /* Inputs: n1: 1st number (float)
+               oper: arit operator 
+               n2: 2nd number (float)
+       Outputs: n1 oper n2 = result (float)
+
+    print "$ ./ Calculadora":
+    print " ":
+    print "Este programa hace operaciones basicas entre dos numeros.":
+    print "Primer numero": n1:
+    print "Operacion": oper:
+    print "Segundo numero": n2
+    print " ":
+    print "Resultado:":
+    print n1 oper n2 "=" resultado
+return 0;
+*/
+
 #include<iostream>
+
+using namespace std;
+
+int main ()
+{
+    float num1 = 0;
+    int   n1 = (int)num1;
+    float num2 = 0;
+    int n2 = (int)num2;
+    float result = 0;
+    char oper = '+';
+
+    cout << "$" << " "<< "./" <<"Calculadora" <<endl <<endl;
+    cout << "Este programa hace operaciones basicas entre dos numeros."<<endl;
+    cout << "Primer numero:" << " ";
+    cin >> num1; 
+    cout << "Operacion:" << " ";
+    cin >> oper;
+    cout << "Segundo numero:" << " ";
+    cin >> num2;
+    cout << " " << endl << endl; 
+    cout << "Resultado:" << endl;
+    if(oper = '+'){
+        cout << num1 << " " << oper << " " << num2 << " " << "=" <<" " << num1 + num2 << endl;
+    }
+    if(oper = '-'){
+        cout << num1 << " " << oper << " " << num2 << " " << "=" <<" " << num1 - num2 << endl;
+    }
+    if(oper = '*'){
+        cout << num1 << " " << oper << " " << num2 << " " << "=" <<" " << num1 * num2 << endl;
+    }
+    if(oper = '/'){
+        cout << num1 << " " << oper << " " << num2 << " " << "=" <<" " << num1 / num2 << endl;
+    }
+    if(oper = '%'){
+        cout << num1 << " " << oper << " " << num2 << " " << "=" <<" " << n1 % n2 << endl;
+    }
+    return 0;
+    
+}
+
+
+
+
+
+
+
+/*#include<iostream>
 
 using namespace std;
 
@@ -49,4 +117,4 @@ int main(){
 
     }
     return 0;
-}
+}*/
