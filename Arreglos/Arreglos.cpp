407a1deb775c36433432e273f3b9435e0ee6eee7
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-09-23 09:43:22 -0500

    Try no.1
    We deleted arr1 and wrote arr in its place

--- Arreglos/Arreglos.cpp
+++ Arreglos/Arreglos.cpp
@@ -17,14 +17,14 @@ using namespace std; //TO SIMPLIFY FUNCITON CALLING
 void ImprimirArreglo(array<float, LEN> arreglo);
 array<float, LEN> LlenarArreglo(void);
 array<float, LEN> MultiplicarArreglo(array<float, LEN> arreglo, float multiplo);
-array<float, LEN> SumarArreglos(array<float, LEN> arr1, array<float, LEN>arr2);
+array<float, LEN> SumarArreglos(array<float, LEN> arr, array<float, LEN>arr2);
 
 ///////////////////////////////////////////////////// MAIN FUNCTION ///////////////////////////////////////////////////////////////////////////
 
 int main()
 {
     char decision;
-    array<float, LEN> arr = { 0 };  // INITIALIZING THE ARRAY WITH 0
+    array<float, LEN> arr = { 0 };  // START THE ARRAY WITH 0
     arr = LlenarArreglo(); // ASKING THE USER TO FILL AN ARRAY; WE ASIGN IT TO OUR MAIN ARRAY
     cout << "Operación: ";
     cin >> decision;
@@ -47,11 +47,11 @@ int main()
         arr2 = LlenarArreglo(); // WE AKS THE USER TO FILL THE 2ND ARRAY; WE ASIGN IT TO OUR NEW ARRAY
         arr = SumarArreglos(arr, arr2); // WE ADD BOTH ARRAYS ELEMENT TO ELEMENT; WE REASIGN THE RESULT TO MAIN ARRAY
         }
-        cout << "Resultado: ";
-        ImprimirArreglo(arr); // WE PRINT THE RESULT OF THE OP. OVER THE MAIN ARRAY
-        return 0;
+    cout << "Resultado: ";
+    ImprimirArreglo(arr); // WE PRINT THE RESULT OF THE OP. OVER THE MAIN ARRAY
+    return 0;
 }
-
+//////////////////////////////////////////////////////// SECONDARY FUNCTIONS /////////////////////////////////////////////////////////////////
 // PRINT THE ELEMENTS OF 'arreglo' ARRAY WITH AN SPACE BETWEEN ELEMENTS
     void ImprimirArreglo(array<float, LEN> arreglo)
     {
@@ -60,20 +60,24 @@ int main()
         cout << '\n';
     }
 
-// FILL AN LENGTH = LEN ARRAY AND RETURN IT AS RESULT
+// FILL A LENGTH = LEN ARRAY AND RETURN IT AS RESULT
 array<float, LEN>LlenarArreglo()
     {
-        //ALL
+        for (int i = 0; i < LEN; i++){
+            cin >> arr[LEN];
+        }
     }
 
 // MULTIPLY EACH ELEMENT OF 'arreglo' ARRAY BY FACTOR 'mult'
 array<float, LEN> MultiplicarArreglo(array<float, LEN> arreglo, float multiplo)
     {
-        //ALL
+        arr = arreglo * multiplo;
+        return (arr);
     }
 
 // ADD EACH ELEMENT OF 'arr1' & 'arr2' AND RETURN RESULT ARRAY
-array<float, LEN> SumarArreglos(array<float, LEN> arr1, array <float, LEN> arr2)
+array<float, LEN> SumarArreglos(array<float, LEN> arr, array <float, LEN> arr2)
     {
-        // ALL
+        arr = arr + arr2;
+        return (arr);
     }
