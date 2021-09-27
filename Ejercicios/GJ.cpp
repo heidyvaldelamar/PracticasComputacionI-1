e1260bcecc59d23e43af0754bfd34cbbaf6444df
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-09-27 11:44:50 -0500

    Clase GJ
    Falta eliminación;
    resolver impresión de la matriz

--- Ejercicios/GJ.cpp
+++ Ejercicios/GJ.cpp
@@ -13,32 +13,64 @@ int A[3][4]{{2,1,-1,8},
             {-3,-1,2,-11}, 
             {-2, 1, 2, -3}};
 
-int encontrarPivote(int row, int col);
+int encontrarPivote(int fila, int col);
+void intercambiarFilas(int f1, int f2);
+void imprimirMatriz(void);
 
 int main(){
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 3 ; j++){
             int pivote = A[i][i]; // diagonal principal
-            if (pivote != 0){
-                // Realizar eliminación
+            if (pivote == 0){
+                imprimirMatriz();
+               int nuevaFila = encontrarPivote(i, j);
+                if (nuevaFila == -1){
+                    cout << "El sistema de ecuaciones no tiene solucion" <<endl;
+                    exit(0);
+                }
+                else{
+                    //Realizar intercambio de filas (i, nuevaFila)
+                    intercambiarFilas(i, nuevaFila);
+                    pivote = A[i][i];
+                    imprimirMatriz();
+                }
             }
-            else{
-
-            }
+            // Realizar eliminacion
+            // TODO:
+            // 1) multiplicar
+            
         }
     }
     return 0;
 }
 
-int encontrarPivote(int row, int col){
+int encontrarPivote(int fila, int col){
     int max = -999999;
     int indiceFila = -1;
-    for (int j = row + 1; j<filas; j++){
-        if(A[j][col] > max){
+    for (int j = fila + 1; j<filas; j++){
+        if(A[j][col] > max && A[j][col] != 0){
             max = A[j][col];
             indiceFila = j;
         }
         
     }
     return indiceFila;
+}
+
+void intercambiarFilas(int f1, int f2){ // cambio elemento por elemento
+    int aux[cols];
+    for(int i = 0; i < cols; i++){
+        aux[i] = A[f1][i]; // arreglo[fila][col]
+        A[f1][i] = A[f2][i];
+        A[f2][i] = aux[i];
+    }
+}
+
+void imprimirMatriz(void){
+    for (int i = 0; i < filas; i++){
+        for(int j = 0; j < cols; j++){
+            cout << A[i][j] << "  " ;
+            cout << endl;
+        }
+    }
 }
