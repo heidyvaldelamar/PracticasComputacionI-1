426599b7cee6ad126eedd374f3d39e686ff867a0
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-09-27 10:27:59 -0500

    Plantillas de Funciones
    27 SEP 2021

---
+++ PlatillaDeFunciones.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+// SOBRECARGA DE FUNCIONES
+int sum(int a, int b);
+float sum(float a, float b);
+
+int main(){
+    int a = 1, b = 3;
+    float c = 0.5, d = 4.5;
+    cout << "La suma de dos enteros es: " << sum(a,b)<< endl;
+    cout << "La suma de dos flotantes es: " << sum(c,d) <<endl;
+}
+
+int sum(int num1, int num2){
+    return num1 + num2;
+}
+
+float sum(float a, float b){
+    return a + b;
+}
