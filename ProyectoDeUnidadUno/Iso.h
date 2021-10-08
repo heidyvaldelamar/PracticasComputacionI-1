8557965814664fe2f8c428460f48c8d5dfe26930
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-07 19:18:42 -0500

    1

--- ProyectoDeUnidadUno/Iso.h
+++ ProyectoDeUnidadUno/Iso.h
@@ -2,10 +2,11 @@
 */
 
 // Calse Iso
+#include <iostream>
+using namespace std;
 
 class Iso
 {
-    public:{
-        double CalculaPuntoIso(double pka1, double pka2)
-    };
+    public:
+        double CalculaPuntoIso(double pka1, double pka2);
 };
--- ProyectoDeUnidadUno/PuntoIso.cpp
+++ ProyectoDeUnidadUno/PuntoIso.cpp
@@ -2,3 +2,21 @@
 */
 
 // Punto Isoeléctrico
+
+#include<iostream>
+#include "Iso.h" // incluimos nuestra clase Iso
+using namespace std; 
+
+double Iso::CalculaPuntoIso(double pka1, double pka2)
+{
+    cout << "Ingrese las constantes de acidez" <<endl;
+    cout << "pka1: ";
+    cin >> pka1;
+    cout << "pka2: "; 
+    cin >> pka2;
+
+    double CalculaPuntoIso(double pka1, double pka2){
+        double pI = (pka1 + pka2) / 2;
+        return pI;
+    }
+}
