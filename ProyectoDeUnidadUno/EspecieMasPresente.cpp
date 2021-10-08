ab39690f62d3d0a71a2f0a1d8e557461a01a9da7
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-07 21:17:47 -0500

    EspecieMasPresente
    Primera parte

--- ProyectoDeUnidadUno/EspecieMasPresente.cpp
+++ ProyectoDeUnidadUno/EspecieMasPresente.cpp
@@ -2,3 +2,9 @@
 */
 
 // Especie más presente
+
+#include "MasPresente.h"
+    double MasPresente::EspecieMasPresente(double pka, double pH)
+    {
+        
+    }
---
+++ ProyectoDeUnidadUno/MasPresente.h
@@ -0,0 +1,10 @@
+/*
+*/
+
+// Clase MasPresente
+
+class MasPresente
+{
+    public:
+        double EspecieMasPresente(double pka, double pH);
+};
