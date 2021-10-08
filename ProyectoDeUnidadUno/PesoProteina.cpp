5e0d36b7dc2a717150366772463083b543d5845e
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-07 21:33:48 -0500

    PesoProteina
    Primera parte

--- ProyectoDeUnidadUno/PesoProteina.cpp
+++ ProyectoDeUnidadUno/PesoProteina.cpp
@@ -2,3 +2,9 @@
 */
 
 // Para saber el peso de una proteína
+#include "Proteina.h"
+
+/*double Proteina::CalculaPesoProteina(array<double, n> Amino)
+{
+    //double PesoProteina =
+};*/
---
+++ ProyectoDeUnidadUno/Proteina.h
@@ -0,0 +1,8 @@
+/*
+*/
+
+class Proteina
+{
+    public:
+        double CalculaPesoProteina(array<double, n> Amino)
+}
