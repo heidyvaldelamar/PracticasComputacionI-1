b45c3000664d0676db298bea699387a0a4c1471c
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-07 20:53:46 -0500

    CalculaPH
    Lista la función CalculaPH
    Falta cómo obtener los datos

--- ProyectoDeUnidadUno/CalculaPH.cpp
+++ ProyectoDeUnidadUno/CalculaPH.cpp
@@ -3,3 +3,11 @@
 
 // Para la función pH
 
+#include "PH.h"
+#include<math.h>
+
+double CalculaPH(double VolSusAg, double MolSusAg, double VolAg)
+{
+    double pH = -log(((MolSusAg * VolSusAg) / (VolSusAg + VolAg)) + 0.0000001);
+    return pH;
+}
