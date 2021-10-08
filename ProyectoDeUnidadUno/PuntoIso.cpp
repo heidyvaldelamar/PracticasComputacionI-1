9b0d5abdd984f1f6d22d1903f8a0a5d2dfa8bb02
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-07 19:30:49 -0500

    CalculaPuntoIso
    Escribimos la función

--- ProyectoDeUnidadUno/PuntoIso.cpp
+++ ProyectoDeUnidadUno/PuntoIso.cpp
@@ -9,14 +9,5 @@ using namespace std;
 
 double Iso::CalculaPuntoIso(double pka1, double pka2)
 {
-    cout << "Ingrese las constantes de acidez" <<endl;
-    cout << "pka1: ";
-    cin >> pka1;
-    cout << "pka2: "; 
-    cin >> pka2;
-
-    double CalculaPuntoIso(double pka1, double pka2){
-        double pI = (pka1 + pka2) / 2;
-        return pI;
-    }
+    double pI = (pka1 + pka2) / 2;
 }
