43eaee480255fd14c7cb7e52a768b10b7922e1bb
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-12 23:35:19 -0500

    3 docs

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,2 +1,16 @@
 /*
+author: SOFIA PALACIOS CUEVAS
+e-mail: sofiapalacioscuevas@comunidad.unam.mx
+  date: OCT 12 2021
 */
+
+class Punto2D
+{
+    public:
+        float SetPosicion( float x, float y);
+        float Trasladar(float temp1, float temp2);
+        float RotarRespectoAlOrigen( float temp1);
+        float Escalar (float temp1, float temp2);
+        float GetX();
+        int GetY();
+};
