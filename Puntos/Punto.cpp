5c3e203eb5fd6f113fb83346f166aa647530b503
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-15 20:26:10 -0500

    Readyyyyyyyyyyyyyyyy
    Se me fue y puse MiX en vez de MiY;
    
    borré cout "x:" y cout "y:"

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -8,7 +8,7 @@ e-mail: sofiapalacioscuevas@comunidad.unam.mx
 // #include "corecrt_math.h" // para cos y sin <- no la encontró, usemos otra
 #include <cmath>
 #include <iostream>
-#define Pi 3.141592
+#define Pi 3.14159265358979323846
 
 using namespace std;
 
@@ -16,9 +16,7 @@ using namespace std;
 float Punto2D::SetPosicion(float x, float y){
   // We set our intial x and y values of our 2D point
   MiX = x; 
-  cout << "x: ";
   MiY = y;
-  cout << "y: ";
   return 0;
   
 }
@@ -32,8 +30,8 @@ float Punto2D::Trasladar(float temp1, float temp2){
 
 float Punto2D::RotarRespectoAlOrigen(float temp1){
   rot = MiX; 
-  MiX = (MiX * cos((temp1*Pi)/180)) - (MiY * sin((temp1*Pi)/180));
-  MiX = (MiX * sin((temp1*Pi)/180)) + (MiY * cos((temp1*Pi)/180));
+  MiX = (rot * cos((temp1*Pi)/180)) - (MiY * sin((temp1*Pi)/180));
+  MiY = (rot * sin((temp1*Pi)/180)) + (MiY * cos((temp1*Pi)/180));
   return 0;
 }
 
