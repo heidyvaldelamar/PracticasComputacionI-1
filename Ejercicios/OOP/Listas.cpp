6bc3a6f0faf26eeaa68c705803c5c8069eed2def
JBSspc <77463414+JBSspc@users.noreply.github.com>
2021-10-18 10:46:33 -0500

    Create Listas.cpp
    Ejercicio en clase de listas

---
+++ Ejercicios/OOP/Listas.cpp
@@ -0,0 +1,26 @@
+/*
+author:
+e-mail:
+  date:
+*/
+
+using namespace std;
+
+
+int main(){
+    list Nodo1;
+    list Nodo2;
+    list Nodo3;
+
+    Nodo1.actual = 1;
+    Nodo2.actual = 2;
+    Nodo3.actual = 3;
+
+    // Generamos una referencia al siguiente nodo.
+    Nodo1.siguiente = &Nodo2.actual;
+    Nodo2.siguiente = &Nodo3.actual;
+    Nodo3.siguiente = &Nodo1.actual;
+
+
+    return 0;
+}
